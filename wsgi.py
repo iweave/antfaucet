15a1c5d130ed50c37fc3f57aef0dd6062bda0e52
Troy Johnson <troy@iweave.sh>
2025-01-28 18:46:17 +0000

    When using wsgi, make sure we prepare the database.

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from main import app
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    # Initialize database
+    if prepare_faucet_database(faucetdb):
+        app.run()
+    else:
+        print("Program terminated due to issues with price database")
